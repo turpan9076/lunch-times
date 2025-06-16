158a81168293dc1de27d428345e9aebe98fcabd1
sopuranoaruto <naka24_0310_it@icloud.com>
2025-06-16 10:59:51 +0900

    売り切れ報告処理(動作確認まだ)

---
+++ pg-insert.php
@@ -0,0 +1,43 @@
+<?php
+    $db_set = "host = localhost user = team6 dbname = team6db password = hirano";
+    $db_conn = pg_connect($db_set);
+    if(!$db_conn){
+        print("接続エラー\n");
+    }else{
+        print("接続完了\n");
+    }
+
+    $r_id = 1;
+    $m_id = 1;
+    $datetime = new DateTime();
+
+    $query = "INSERT INTO report(report_id,menu_id,report_time) VALUES ($r_id,$m_id,$datetime)";
+    $insert = pg_query($db_conn,$query); 
+    if(!$insert){
+        print("Insert error\n");
+    }else{
+        print("Insert success\n");
+    }
+
+    $query = "SELECT * FROM report";
+    $result = pg_query($db_conn,$query);
+    if(!$result){
+        print("result error\n");
+    }else{
+        print("result success\n");
+        $rows = pg_fetch_array($result,NULL,PGSQL_ASSOC);
+        for($i=0;$i<pg_num_rows($result);$i++){
+            printf("report_id=".$rows["report_id"]);
+            printf("|menu_id=".$rows["menu_id"]);
+            printf("|report_time=".$rows["report_time"]);
+        }
+    }
+
+    $db_close = pg_close($db_conn);
+    if($db_close){
+        print("切断完了\n");
+    }else{
+        print("切断失敗\n");
+    }
+
+?>
