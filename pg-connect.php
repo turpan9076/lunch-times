f9d3296d07b812089d23c34419e3b24f7a2b44f4
sopuranoaruto <naka24_0310_it@icloud.com>
2025-06-16 09:18:50 +0900

    クエリ実行＋データ取得(仮)

--- pg-connect.php
+++ pg-connect.php
@@ -6,7 +6,17 @@
     }else{
         print("接続完了\n");
     }
-    
+
+    sleep(1);
+    $query = "SELECT A FROM B";
+    $result = pg_query($db_conn,$query); 
+    if(!$result){
+        print("クエリ実行エラー");
+    }else{
+        print("クエリ実行成功");
+    }
+    sleep(1);
+
     $db_close = pg_close($db_conn);
     if($db_close){
         print("切断完了\n");
