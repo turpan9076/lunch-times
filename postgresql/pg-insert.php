5d03b7834717cbfc15894b23706d5f930194a775
sopuranoaruto <naka24_0310_it@icloud.com>
2025-06-23 11:00:38 +0900

    コメント消しちゃったので修正

--- postgresql/pg-insert.php
+++ postgresql/pg-insert.php
@@ -1,5 +1,9 @@
 <?php
+    /*データベースに接続＋データベースに要素を追加*/
+
+    //データベースをセット
     $db_set = "host = localhost user = team6 dbname = team6db password = hirano";
+    //データベースと接続
     $db_conn = pg_connect($db_set);
     if(!$db_conn){
         print("接続エラー\n");
@@ -7,9 +11,11 @@
         print("接続完了\n");
     }
     
+    //追加するデータを設定
     $m_id = 37;
     $datetime = date("Y-m-d H:i:s");;
 
+    //データを追加
     $query = "INSERT INTO report (menu_id,report_time) VALUES ($1,$2)";
     $insert = pg_query_params($db_conn,$query,array($m_id,$datetime)); 
     if(!$insert){
@@ -18,6 +24,8 @@
         print("Insert success\n");
     }
     
+    /*
+    //データベースのreportテーブルからデータを読み取る
     $query = "SELECT * FROM report";
     $result = pg_query($db_conn,$query);
     if(!$result){
@@ -31,7 +39,9 @@
             printf("|report_time=".$rows["report_time"]);
         }
     }
+    */
 
+    //データベースとの通信を切断
     $db_close = pg_close($db_conn);
     if($db_close){
         print("切断完了\n");
